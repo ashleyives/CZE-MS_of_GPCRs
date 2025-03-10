d4e3220fcdd286a939428c584248251d57b2553f
ashleyives <ashley.ives@pnnl.gov>
2025-03-10 16:30:22 -0700

    making graphs more readable

--- Scripts/unidec_statistics.R
+++ Scripts/unidec_statistics.R
@@ -8,6 +8,8 @@ library(ggpubr)
 data <- read.csv("C:/Users/ives435/OneDrive - PNNL/Desktop/GPCR paper/unidec_stats.csv")
 
 palette <- c("darkgreen", "darkred", "darkblue")
+boxsize <- 1
+psize <- 8
 
 #unmod 
 unmod <- data %>%
@@ -18,13 +20,13 @@ my_comparisons <- list(
    c("1", "3")
 )
 
-unm <- ggboxplot(
+unm <- ggboxplot(size = boxsize,
    unmod, x = "Peak", y = "Height",
    color = "Peak", palette = palette,
-   add = "jitter"
+   add = "dotplot"
 )+
-   stat_compare_means(comparisons = my_comparisons, method = "t.test", label = "p.signif")+
-   labs(x="Migration time region", y = "Relative Intensity (UniDec)", title="Unmodified", color = "Region")+
+   stat_compare_means(comparisons = my_comparisons, method = "t.test", label = "p.signif", size=psize)+
+   labs(x="Migration time region", y = "Relative Intensity (UniDec)", title="Unmodified", color = "Region", fill="Region")+
    theme_classic(base_size = 24)+
    ylim(50,120)
 
@@ -37,13 +39,13 @@ my_comparisons <- list(
    c("1", "3")
 )
 
-mp <- ggboxplot(
+mp <- ggboxplot(size = boxsize,
    monophos, x = "Peak", y = "Height",
    color = "Peak", palette = palette,
-   add = "jitter"
+   add = "dotplot"
 )+
-   stat_compare_means(comparisons = my_comparisons, method = "t.test", label = "p.signif")+
-   labs(x="Migration time region", y = "Relative Intensity (UniDec)", title="Monophosphorylated", color = "Region")+
+   stat_compare_means(comparisons = my_comparisons, method = "t.test", label = "p.signif", size=psize)+
+   labs(x="Migration time region", y = "Relative Intensity (UniDec)", title="Monophosphorylated", color = "Region", fill="Region")+
    theme_classic(base_size = 24)+
    ylim(95,105)
 
@@ -57,13 +59,13 @@ my_comparisons <- list(
    c("1", "3")
 )
 
-dp <- ggboxplot(
+dp <- ggboxplot(size = boxsize,
    diphos, x = "Peak", y = "Height",
    color = "Peak", palette = palette,
-   add = "jitter"
+   add = "dotplot"
 )+
-   stat_compare_means(comparisons = my_comparisons, method = "t.test", label = "p.signif")+
-   labs(x="Migration time region", y = "Relative Intensity (UniDec)", title="Diphosphorylated", color = "Region")+
+   stat_compare_means(comparisons = my_comparisons, method = "t.test", label = "p.signif", size=psize)+
+   labs(x="Migration time region", y = "Relative Intensity (UniDec)", title="Diphosphorylated", color = "Region", fill="Region")+
    theme_classic(base_size = 24)+
    ylim(25,55)
 dp
@@ -78,13 +80,13 @@ my_comparisons <- list(
    c("1", "3")
 )
 
-ac <- ggboxplot(
+ac <- ggboxplot(size = boxsize,
    acyl, x = "Peak", y = "Height",
    color = "Peak", palette = palette,
-   add = "jitter"
+   add = "dotplot"
 )+
-   stat_compare_means(comparisons = my_comparisons, method = "t.test", label = "p.signif")+
-   labs(x="Migration time region", y = "Relative Intensity (UniDec)", title="Palmitoyl", color = "Region")+
+   stat_compare_means(comparisons = my_comparisons, method = "t.test", label = "p.signif", size=psize)+
+   labs(x="Migration time region", y = "Relative Intensity (UniDec)", title="Palmitoyl", color = "Region", fill="Region")+
    theme_classic(base_size = 24)+
    ylim(70,95)
 ac
